26092d7a3219e266417547d28ab6c6f36c9d8260
Tomas Fernandez <tomasfernandez1212@gmail.com>
2023-02-15 19:47:28 -0800

    Message input changes

--- components/chat-component.tsx
+++ components/chat-component.tsx
@@ -9,21 +9,21 @@ import {
 const ChatComponent = () => {
   return (
     <MainContainer style={{fontSize: "1.2em"}}>
-    <ChatContainer>
-      <MessageList>
-        <Message
-          model={{
-            message: "Customer service: Hello there! How can I help you?",
-            sentTime: "just now",
-            sender: "Joe",
-            direction: "incoming",
-            position: "single",
-          }}
-        />
-      </MessageList>
-      <MessageInput placeholder="Type message here" onKeyDown={(event) => event.stopPropagation()}/>
-    </ChatContainer>
-  </MainContainer>
+      <ChatContainer>
+        <MessageList>
+          <Message
+            model={{
+              message: "Customer service: Hello there! How can I help you?",
+              sentTime: "just now",
+              sender: "Joe",
+              direction: "incoming",
+              position: "single",
+            }}
+          />
+        </MessageList>
+        <MessageInput placeholder="Type message here" sendButton={true} autoFocus={true} onKeyDown={(event) => event.stopPropagation()}/>
+      </ChatContainer>
+    </MainContainer>
   );
 }
 
