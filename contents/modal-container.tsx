845322a0c226f3965d36589ba4513ffb201105b7
Tomas Fernandez <tomasfernandez1212@gmail.com>
2023-02-17 15:24:56 -0800

    Button on bottom right

--- contents/modal-container.tsx
+++ contents/modal-container.tsx
@@ -6,6 +6,9 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import bootstrapStyles from "data-text:bootstrap/dist/css/bootstrap.min.css";
 
+// To override styles, need to use in plasmo config and get style
+import modalContainerStyles from "data-text:./modal-container.css";
+
 import ChatComponent from "../components/chat-component"
 import chatKitStyles from "data-text:@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 
@@ -18,53 +21,40 @@ export const config: PlasmoCSConfig = {
 // Inject into the ShadowDOM
 export const getStyle: PlasmoGetStyle = () => {
   const style = document.createElement("style")
-  style.textContent = bootstrapStyles
+  style.textContent = bootstrapStyles + modalContainerStyles
   return style
-}
-
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      animation={false}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
 }
 
 
 const DataGenieModal = () => {
-  const [modalShow, setModalShow] = useState(true);
+  const [modalShow, setModalShow] = useState(false);
 
   return (
     <>
-      <Button variant="primary" onClick={() => setModalShow(true)}>
-        Launch vertically centered modal
+      <Button id="datagenie-modal-button" variant="primary" onClick={() => setModalShow(!modalShow)}>
+        {modalShow ? "Close" : "Open"}
       </Button>
 
-      <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
+      <Modal show={modalShow} onHide={() => setModalShow(false)}
+      aria-labelledby="contained-modal-title-vcenter" animation={false}
+      dialogClassName="datagenie-modal">
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => setModalShow(false)}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </>
 )}
 
