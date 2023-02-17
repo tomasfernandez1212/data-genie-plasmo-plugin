4e15c525a2e67884fab19432831688a16655c871
Tomas Fernandez <tomasfernandez1212@gmail.com>
2023-02-16 18:28:00 -0800

    Bootstrap shows. The key was to set animation to False.

--- contents/modal-container.tsx
+++ contents/modal-container.tsx
@@ -1,40 +1,74 @@
 import type { PlasmoGetStyle } from "plasmo"
 import type { PlasmoCSConfig } from "plasmo"
-import { useEffect, useState } from "react"
+import { useEffect, useState} from "react"
 
 import ChatComponent from "../components/chat-component"
 import collapseWindowStyle from "data-text:~/contents/modal-container.css"
 import chatKitStyles from "data-text:@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import bootstrapStyles from "data-text:bootstrap/dist/css/bootstrap.min.css";
 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+// Import React
+import React from "react"
 
 // Match any URL that ends with ipynb
 export const config: PlasmoCSConfig = {
   matches: ["*://*/*.ipynb"],
 }
 
-
 // Inject into the ShadowDOM
 export const getStyle: PlasmoGetStyle = () => {
   const style = document.createElement("style")
-  style.textContent = collapseWindowStyle + chatKitStyles
+  style.textContent = bootstrapStyles
   return style
 }
 
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      animation={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+
 const DataGenieModal = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [modalShow, setModalShow] = React.useState(true);
 
   return (
-    <div className="modalContainer">
-      {isOpen && (
-        <div className="modal">
-          <ChatComponent />
-        </div>
-      )}
-      <button className="modalButton" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? 'Close' : 'Open'}
-      </button>
-    </div>
-  )
-}
+    <>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+)}
 
 export default DataGenieModal
