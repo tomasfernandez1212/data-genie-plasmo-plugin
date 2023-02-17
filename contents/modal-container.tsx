a61329b3df2a5accd8e0c9435df5a6f182529cad
Tomas Fernandez <tomasfernandez1212@gmail.com>
2023-02-16 16:37:09 -0800

    Trying to change to a bootstrap modal, but not working well with plasmo. Button renders, but then modal doesn't show up although it is affecting the DOM

--- contents/modal-container.tsx
+++ contents/modal-container.tsx
@@ -5,7 +5,10 @@ import { useEffect, useState } from "react"
 import ChatComponent from "../components/chat-component"
 import collapseWindowStyle from "data-text:~/contents/modal-container.css"
 import chatKitStyles from "data-text:@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import bootstrapStyles from 'data-text:bootstrap/dist/css/bootstrap.min.css';
 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 // Match any URL that ends with ipynb
 export const config: PlasmoCSConfig = {
@@ -16,23 +19,39 @@ export const config: PlasmoCSConfig = {
 // Inject into the ShadowDOM
 export const getStyle: PlasmoGetStyle = () => {
   const style = document.createElement("style")
-  style.textContent = collapseWindowStyle + chatKitStyles
+  style.textContent = bootstrapStyles + collapseWindowStyle + chatKitStyles
+  console.log(style.textContent)
   return style
 }
 
 const DataGenieModal = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  console.log(show)
 
   return (
-    <div className="modalContainer">
-      {isOpen && (
-        <div className="modal">
-          <ChatComponent />
-        </div>
-      )}
-      <button className="modalButton" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? 'Close' : 'Open'}
-      </button>
+    <div>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}> 
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
