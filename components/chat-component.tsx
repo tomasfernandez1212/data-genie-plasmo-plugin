dd4b3f03293a22b5035d58705aa3af9634c15d48
Tomas Fernandez <tomasfernandez1212@gmail.com>
2023-02-18 15:58:59 -0800

    Added content within the messaging modal. Added datagenie thumbnail.

--- components/chat-component.tsx
+++ components/chat-component.tsx
@@ -1,19 +1,38 @@
 import {
   MainContainer,
+  ConversationHeader,
   ChatContainer,
   MessageList,
+  TypingIndicator,
   Message,
+  Avatar,
   MessageInput,
 } from "@chatscope/chat-ui-kit-react";
 
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 
+import dataGenieThumbnail from "../assets/datagenie-thumbnail.png"
 
 const ChatComponent = () => {
   return (
-    <MainContainer> 
+    <MainContainer responsive={true}> 
       <ChatContainer>
-        <MessageList>
+        <ConversationHeader>
+              <Avatar src={dataGenieThumbnail} name="Data Genie" />
+              <ConversationHeader.Content>
+                <span >Data Genie</span>
+              </ConversationHeader.Content>
+          </ConversationHeader>
+        <MessageList typingIndicator={<TypingIndicator content="Eliot is typing" />}>
+          <Message
+            model={{
+              message: "Customer service: Hello there! How can I help you?",
+              sentTime: "just now",
+              sender: "Joe",
+              direction: "incoming",
+              position: "single",
+            }}
+          />
           <Message
             model={{
               message: "Customer service: Hello there! How can I help you?",
@@ -24,7 +43,7 @@ const ChatComponent = () => {
             }}
           />
         </MessageList>
-        <MessageInput placeholder="Type message here" sendButton={true} autoFocus={true} onKeyDown={(event) => event.stopPropagation()}/>
+        <MessageInput placeholder="Type message here" attachButton={false} sendButton={true} autoFocus={true} onKeyDown={(event) => event.stopPropagation()}/>
       </ChatContainer>
     </MainContainer>
   );
