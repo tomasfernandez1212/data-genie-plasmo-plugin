1fc3d4847c71a2ad0b574eab031beccf0a22ba7c
Tomas Fernandez <tomasfernandez1212@gmail.com>
2023-02-18 17:33:20 -0800

    Moved message list to state

--- components/chat-component.tsx
+++ components/chat-component.tsx
@@ -9,6 +9,7 @@ import {
   Message,
   Avatar,
   MessageInput,
+  MessageModel
 } from "@chatscope/chat-ui-kit-react";
 
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
@@ -16,9 +17,17 @@ import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import dataGenieThumbnail from "../assets/datagenie-thumbnail.png"
 
 const ChatComponent = () => {
-
   
   const [isTyping, setIsTyping] = useState(true)
+  const [messages, setMessages] = useState<MessageModel[]>([
+    {
+      message: "Hello there! How can I help you?",
+      sentTime: "just now",
+      sender: "Data Genie",
+      direction: "incoming",
+      position: "single",
+    }
+  ]);
 
   return (
     <MainContainer responsive={true}> 
@@ -30,24 +39,9 @@ const ChatComponent = () => {
             </ConversationHeader.Content>
         </ConversationHeader>
         <MessageList typingIndicator={isTyping ? <TypingIndicator content="Data Genie is typing" /> : null}>
-          <Message
-            model={{
-              message: "Customer service: Hello there! How can I help you?",
-              sentTime: "just now",
-              sender: "Joe",
-              direction: "incoming",
-              position: "single",
-            }}
-          />
-          <Message
-            model={{
-              message: "Customer service: Hello there! How can I help you?",
-              sentTime: "just now",
-              sender: "Joe",
-              direction: "incoming",
-              position: "single",
-            }}
-          />
+          {messages.map((message, index) => (
+            <Message key={index} model={message} />
+          ))}
         </MessageList>
         <MessageInput placeholder="Type message here" attachButton={false} sendButton={true} autoFocus={true} onKeyDown={(event) => event.stopPropagation()}/>
       </ChatContainer>
