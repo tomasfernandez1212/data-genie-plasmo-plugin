3b0c2c720a77f1bdedb48206863aeadc2e8c0b84
Tomas Fernandez <tomasfernandez1212@gmail.com>
2023-03-05 12:47:50 -0800

    Jupyter being accessed from background connection with content script via port!

--- background/ports/jupyter-background.ts
+++ background/ports/jupyter-background.ts
@@ -1,9 +1,40 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
+function getCurrentTabId() {
+    return new Promise<number>((resolve, reject) => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        const tab = tabs[0];
+        if (!tab) {
+          reject(new Error("No active tab found"));
+          return;
+        }
+        const tabId = tab.id;
+        resolve(tabId);
+      });
+    });
+}
+
+
 const handler: PlasmoMessaging.PortHandler = async (req, res) => {
+
     console.log(req)
-   
     res.send("Hello from port handler")
+
+    getCurrentTabId().then((tabId) => {
+        chrome.scripting.executeScript(
+            {
+                target: {tabId},
+                world: "MAIN", // MAIN in order to access the window object
+                func: () => {console.log("Jupyter", (window as any).Jupyter)},
+            },
+            () => {
+                console.log("Background script got callback after injection")
+            }
+        );
+    }).catch((error) => {
+        console.error(error);
+    });
+
 }
    
 export default handler
