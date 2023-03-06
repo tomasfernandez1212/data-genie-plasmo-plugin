354dec0d737d0b8651a3bd6e9def68fb3720a3db
Tomas Fernandez <tomasfernandez1212@gmail.com>
2023-03-05 17:20:42 -0800

    Figured out how to pass arguments to function in chrome script

--- background/ports/jupyter-background.ts
+++ background/ports/jupyter-background.ts
@@ -14,18 +14,28 @@ function getCurrentTabId() {
     });
 }
 
+const handleInstruction = (req, res) => {
+    console.log("handleInstruction");
+    console.log(req);
+    console.log(res);
+    (window as any).Jupyter.notebook.insert_cell_above("code");
+}
+
 
 const handler: PlasmoMessaging.PortHandler = async (req, res) => {
 
     console.log(req)
     res.send("Hello from port handler")
 
+   
+
     getCurrentTabId().then((tabId) => {
         chrome.scripting.executeScript(
             {
                 target: {tabId},
                 world: "MAIN", // MAIN in order to access the window object
-                func: () => {(window as any).Jupyter.notebook.insert_cell_above("code")},
+                func: handleInstruction,
+                args : [req, res]
             },
             () => {
                 console.log("Background script got callback after injection")
--- contents/jupyter-notebook.tsx
+++ contents/jupyter-notebook.tsx
@@ -1,25 +1,14 @@
 // Plasmo Imports
 import type { PlasmoCSConfig } from "plasmo"
 
-// React Imports
-import { useEffect, useState} from "react"
-
 // Match any URL that ends with ipynb
 export const config: PlasmoCSConfig = {
   matches: ["*://*/*.ipynb"],
   world: "MAIN"
 }
 
+// Empty component - leave as is or else causes problems with background port scripts. 
 export default function JupyterNotebook() {
-
-    const [modalShow, setModalShow] = useState(false);
-
-    console.log("modalShow", modalShow)
-
-    if (modalShow) {
-        const Jupyter = (window as any).Jupyter
-        console.log("Jupyter", Jupyter)
-    }
 
     return
 }
